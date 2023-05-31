26c5d067b9e35dd50b5d53ab0551483376e2e15d
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-05-31 16:16:19 +0330

    create components files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 import Profile from "./pages/user/Profile";
-import Offers from "./pages/components/offers/Offers";
+import Offers from "./pages/offers/Offers";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import SignIn from "./pages/auth/SignIn";
 import SignUp from "./pages/auth/SignUp";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
